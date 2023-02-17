1fdacab9e968617800569bcdfd7d2285892331b5
labuser <Chandana>
2023-02-17 12:16:51 +0000

    this keyword

--- BasicPrograms/BasicPrograms/constructors.cs
+++ BasicPrograms/BasicPrograms/constructors.cs
@@ -1,32 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace BasicPrograms
-{
+//namespace BasicPrograms
+//{
 
-    class bike
-    {
-       public bike()
-        {
-            Console.WriteLine("I am royal enfield");
-        }
-        public bike(int cc)//parameterized constructor
-        {
-            Console.WriteLine("I am royal enfield "+cc);
-        }
-    }
-    class constructors
-    {
-        static void Main(string[] args)
-        {
-            bike b = new bike();
-            bike b1 = new bike(650);
+//    class bike
+//    {
+//       public bike()
+//        {
+//            Console.WriteLine("I am royal enfield");
+//        }
+//        public bike(int cc)//parameterized constructor
+//        {
+//            Console.WriteLine("I am royal enfield "+cc);
+//        }
+//    }
+//    class constructors
+//    {
+//        static void Main(string[] args)
+//        {
+//            bike b = new bike();
+//            bike b1 = new bike(650);
             
-            Console.ReadLine();
+//            Console.ReadLine();
 
-        }
-    }
-}
+//        }
+//    }
+//}
