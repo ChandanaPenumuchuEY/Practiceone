0c4bcd2fb90f707209a61240df50b5e0f8e07b99
labuser <Chandana>
2023-02-17 11:02:18 +0000

    public static class

---
+++ BasicPrograms/BasicPrograms/Oopsconcepts.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    class Oopsconcepts
+    {
+        static void Main(string[] args)
+        {
+            demo1 o1 = new demo1();
+            demo2 o2 = new demo2();
+            demo3 o3 = new demo3();
+            o1.func1();
+            o2.func2();
+            o3.func3();
+            demo1.func4();
+            Console.ReadLine();
+        }
+    }
+    public class demo1
+    {
+        public void func1()
+        {
+            Console.WriteLine("fun1 inoked");
+        }
+        public static void func4()
+        {
+            Console.WriteLine("fun4 inoked");
+        }
+    }
+    public class demo2
+    {
+       public void func2()
+        {
+            Console.WriteLine("fun2 inoked");
+        }
+    }
+    public class demo3
+    {
+        public void func3()
+        {
+            Console.WriteLine("fun1 inoked");
+        }
+    }
+}
--- BasicPrograms/BasicPrograms/doapp2.cs
+++ BasicPrograms/BasicPrograms/doapp2.cs
@@ -1,63 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿////using System;
+////using System.Collections.Generic;
+////using System.Linq;
+////using System.Text;
+////using System.Threading.Tasks;
 
-namespace BasicPrograms
-{
-    class doapp2
-    {
-        static void Main(string[] args)
-        {
-            doApp();
-        }
-         static public void doApp()
-        {
-            int i = 10;
-            if (i == 10)
-            {
-                Console.WriteLine(" THIS IS IN IF CONDITION");
-            }
-            switch (i)
-            {
-                case 10:
-                    Console.WriteLine("THIS IS SWITCH case 10 CONDITION");
-                    break;
-                case 9:
-                    Console.WriteLine("THIS IS SWITCH case 9 CONDITION");
-                    break;
-                default:
-                    Console.WriteLine("THIS IS SWITCH default CONDITION");
-                    break;
+////namespace BasicPrograms
+////{
+////    class doapp2
+////    {
+////        static void Main(string[] args)
+////        {
+////            doApp();
+////        }
+////         static public void doApp()
+////        {
+////            int i = 10;
+////            if (i == 10)
+////            {
+////                Console.WriteLine(" THIS IS IN IF CONDITION");
+////            }
+////            switch (i)
+////            {
+////                case 10:
+////                    Console.WriteLine("THIS IS SWITCH case 10 CONDITION");
+////                    break;
+////                case 9:
+////                    Console.WriteLine("THIS IS SWITCH case 9 CONDITION");
+////                    break;
+////                default:
+////                    Console.WriteLine("THIS IS SWITCH default CONDITION");
+////                    break;
 
-            }
-            Console.WriteLine("THIS IS FOR LOOP ITERATION");
-            for (i = 10; i <= 30; i++)
-            {
-                Console.WriteLine(i);
-            }
-            Console.WriteLine("THIS IS WHILE LOOP");
-            while (i >= 20)
-            {
-                Console.WriteLine(i);
-                i--;
-            }
-            Console.WriteLine("THIS IS DO WHILE LOOP");
-            do
-            {
-                Console.WriteLine(i);
-                i--;
+////            }
+////            Console.WriteLine("THIS IS FOR LOOP ITERATION");
+////            for (i = 10; i <= 30; i++)
+////            {
+////                Console.WriteLine(i);
+////            }
+////            Console.WriteLine("THIS IS WHILE LOOP");
+////            while (i >= 20)
+////            {
+////                Console.WriteLine(i);
+////                i--;
+////            }
+////            Console.WriteLine("THIS IS DO WHILE LOOP");
+////            do
+////            {
+////                Console.WriteLine(i);
+////                i--;
 
-            } while (i >= 1);
-            String[] x = { "one", "two", "three" };
-            foreach(String s  in x)
-            {
-                Console.WriteLine(s);
-            }
+////            } while (i >= 1);
+////            String[] x = { "one", "two", "three" };
+////            foreach(String s  in x)
+////            {
+////                Console.WriteLine(s);
+////            }
 
 
-            Console.ReadLine();
-        }
-    }
-}
+////            Console.ReadLine();
+////        }
+////    }
+////}
